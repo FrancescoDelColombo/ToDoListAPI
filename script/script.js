6ca45870e8d2f28ad1b31aba280c77e42574aec6
Francesco Del Colombo <francescodelcolombo2@gmail.com>
2025-07-07 15:39:44 +0200

    API application

--- script/script.js
+++ script/script.js
@@ -3,6 +3,63 @@ document.addEventListener("click", function () {
   document.querySelector("#new-todo").classList.remove("error-input");
 });
 
+const baseUr1 = "https://its-todo-api.azurewebsites.net/api";
+
+/*document.addEventListener("DOMContentLoaded", function () {
+  fetchData().then((todos) => {
+    if (todos) {
+      console.log("Dati Ricevuti: ",todos);
+      renderTodos
+    }
+  })
+})*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetchData().then((todos) => {
+    if (todos) {
+      console.log("Dati ricevuti:", todos);
+      renderTodos(todos)
+    }
+  }).error((error) => {
+    console.error("Errore durante il fetch dei dati:", error);
+  });
+});
+
+async function fetchData() {
+  
+  try{
+    const response = await fetch(`${baseUr1}/ToDo`, {
+      method: "GET",
+      headers:{
+        "x-api-key":"V4PaperYx4Ycc6zucdO6",
+      },
+    });
+
+    if (!response.ok) throw new Error("Errore nella risposta");
+
+    const todos = await response.json();
+    return todos; // Questo ritorna i dati all'interno della funzione async
+  } catch (error) {
+    console.error("Errore API:", error);
+    return null;
+
+    }
+  }
+
+function renderTodos(todos) {
+  todos.forEach((item, i) => {
+    document.querySelector(".todos").insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="todo-container">
+          <div class="todo" id="todo-${i}">${item.description}</div>
+          <input type="checkbox" name="chk-${i}" id="chk-${i}" onclick="clicked('chk-${i}')" />
+        </div>
+        `
+    );
+  });
+}
+
 function addTodo() {
   var todoText = document.querySelector("#new-todo").value;
   const input = document.querySelector("#new-todo");
